2c1b6862c29970102113a876d3c160a7afcd119a
crivmar <carlosrivero1988@gmail.com>
2021-04-16 20:16:01 +0200

    Modificada sintaxis de app en la zona de potencia, daba fallo

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def index():
     return render_template("index.html")
 
 @app.route ('/potencia', methods=['GET','POST'])
-@app.route('/potencia/base/exponente', methods=['GET','POST'])
+@app.route('/potencia/<int:base>/<int:exponente>', methods=['GET','POST'])
 def potencia(base=0,exponente=0):
-    if exponente >=1:
+    if exponente >= 1:
         resultado = base**exponente
-    elif exponente =0:
-        resultado= 1
+    elif exponente == 0:
+        resultado = 1
     elif exponente < 0:
-        resultado= 1/(base**exponente)
+        resultado = 1/(base**exponente)
     return render_template("potencia.html", b=base,e=exponente,r=resultado)
 
 ## Debug ##
