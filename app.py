91898b9d1d3fd4a9405254ff20323708ac5810bb
crivmar <carlosrivero1988@gmail.com>
2021-04-16 20:29:16 +0200

    Depurando el codigo de potencia, pero el exponente negativo no me funciona.

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ def index():
 
 @app.route ('/potencia', methods=['GET','POST'])
 @app.route('/potencia/<int:base>/<int:exponente>', methods=['GET','POST'])
-def potencia(base=0,exponente=0):
+def potencia(base=0,exponente=0,resultado='no definido'):
     if exponente >= 1:
         resultado = base**exponente
-    elif exponente == 0:
+    elif exponente == 0 and base >=1:
         resultado = 1
     elif exponente < 0:
-        resultado = 1/(base**exponente)
+        resultado = 1/base**(exponente*-1)
     return render_template("potencia.html", b=base,e=exponente,r=resultado)
 
 ## Debug ##
